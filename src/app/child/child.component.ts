c945f758303114c6115cf70050faea5d7fcfd9c2
domezochy <domezochy@gmail.com>
2022-03-05 09:58:22 -0600

    DoChange OnChanges

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnChanges, OnDestroy, Input, OnInit, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit, OnDestroy{
+export class ChildComponent implements OnInit, OnDestroy, OnChanges, DoCheck{
 
   counter = 0;
   interval: any;
+  @Input()
   channelName = "";
   
   constructor() { 
@@ -17,18 +18,28 @@ export class ChildComponent implements OnInit, OnDestroy{
 
   ngOnInit() {
     console.log("Child OnInit is called");
-
+/*  
     this.interval = setInterval(() => {
       this.counter = this.counter + 1;
       console.log(this.counter)
     }, 1000);
+*/  
   }
 
   ngOnDestroy() {  //component removed from the DOM.
-    clearInterval(this.interval);
-    console.log("Child OnDestroy is called")
+ //   clearInterval(this.interval);
+    console.log("Child OnDestroy is called");
     
   }
 
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes);
+    console.log("Child onChanges is called");
+  }
+
+  ngDoCheck(){
+  console.log("Child DoCheck is called");
+
+  }
 
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit, OnChanges, DoCheck {
 
-  isChild = false;
+  isChild = true;
+  channelName = '';
 
   constructor() { 
     console.log("Parent Constructor is called")
@@ -15,6 +16,14 @@ export class ParentComponent implements OnInit {
 
   ngOnInit() {
     console.log("Parent OnInit is called")
+  }
+
+  ngOnChanges(){
+    console.log("Parent onChanges is called")
+  }
+
+  ngDoCheck(){
+    console.log("Parent doCheck is called");
   }
 
   toggleChild(){
